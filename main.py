7a16f14def8008ae0a0100d539289fa6e5c968e3
mhou167 <mhou167@wgu.edu>
2025-01-25 13:51:06 -0500

    Step 5 complete, but failing

--- main.py
+++ main.py
@@ -128,12 +128,19 @@ def go(config: DictConfig):
             #pass
 
         if "test_regression_model" in active_steps:
-
             ##################
             # Implement here #
             ##################
-
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                'main',
+                parameters={
+                    "mlfow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest",
+                },
+            )
+            #pass
+            
 
 
 if __name__ == "__main__":
